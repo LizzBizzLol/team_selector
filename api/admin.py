2afe3a2023e24390626d9b7e1037fe1f26692da9
Lissbeth <usacheva.elizaveta@mail.ru>
2025-04-20 20:22:15 +0500

    Очистка ветки от всех файлов

--- api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Skill, Student, StudentSkill, TaskRequirement, Task
-
-admin.site.register(Skill)
-admin.site.register(Student)
-admin.site.register(StudentSkill)
-admin.site.register(TaskRequirement)
-admin.site.register(Task)
