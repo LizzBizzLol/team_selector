9c44d305b156692e3adeb6e79edf19e43cfedb26
Lissbeth <usacheva.elizaveta@mail.ru>
2025-04-20 20:22:15 +0500

    Очистка ветки от всех файлов

--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import create_team, index, upload_team_data, get_all_students
-
-urlpatterns = [
-    path('students/', get_all_students, name='get_all_students'),
-    path('create_team/', create_team, name='create_team'),
-    path('upload_team_data/', upload_team_data, name='upload_team_data'),
-    path('', index, name='index'),
-]
